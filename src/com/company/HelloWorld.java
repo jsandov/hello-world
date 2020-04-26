41fe850e0dfc519dfc5ac13f54d88046ebd27dc0
jsandov <46768834+jsandov@users.noreply.github.com>
2020-04-26 15:32:11 -0700

    Added main method that prints hello world.

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, World!");
+    }
 }
